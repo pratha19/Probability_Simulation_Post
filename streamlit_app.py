b6627f92bda7e8e0190cd381cdcaf687c6c87b51
Prathamesh Pawar <prathamesh.pawar2610@gmail.com>
2022-05-29 19:54:00 -0400

    first commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,19 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
 
+def main():
+    st.title('Simple Streamlit App')
+
+    st.text('Type a number in the box below')
+
+    n = st.number_input('Number', step=1)
+
+    st.write(f'{n} + 1 = {n+1}')
+
+    s = st.text_input('Type a name in the box below')
+
+    st.write(f'Hello {s}')
+    
+if __name__ == '__main__':
+    main()
